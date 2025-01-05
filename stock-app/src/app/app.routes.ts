1dd7fbd031d24879b93118644819003e3003ec9a
Darienn <darlopezp@udd.cl>
2025-01-05 20:20:27 -0300

    Cards Home y Botón "Agregar Item"
    + Se agregaron cards de acceso directo a el stock y las cotizaciones.
    + Se agregó Botón para proximamente añadir funcionalidad de agregar item al stock.

--- stock-app/src/app/app.routes.ts
+++ stock-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StockComponent } from './stock/stock.component';
 
 export const routes: Routes = [
-    {path: '',component:HomeComponent}
+    {path: '',component:HomeComponent},
+    {path: 'stock', component:StockComponent}
 ];
