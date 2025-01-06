f1caffef5b0dd71bed85916265c17d701cac9500
Darienn <darlopezp@udd.cl>
2025-01-05 21:46:01 -0300

    Base Items en Stock
    + Se creo el componente Item para mostrar los items.
    + Se creó filtro (Aún sin funcion)

--- stock-app/src/app/stock/stock.component.ts
+++ stock-app/src/app/stock/stock.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ItemComponent } from '../item/item.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stock',
-  imports: [],
+  imports: [ItemComponent, MatIconModule],
   templateUrl: './stock.component.html',
   styleUrl: './stock.component.css'
 })
