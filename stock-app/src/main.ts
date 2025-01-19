df913bc7e1a7a78eb0542ae368af4acef577240c
Darienn <darlopezp@udd.cl>
2025-01-19 16:42:15 -0300

    Interceptors for csrf token

--- stock-app/src/main.ts
+++ stock-app/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpXsrfInterceptor } from './app/interceptors/http-xsrf.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig, {
+  providers: [
+    provideHttpClient(
+      withInterceptors([HttpXsrfInterceptor]) // Agregar el interceptor
+    ),
+  ],
+}).catch((err) => console.error(err));
